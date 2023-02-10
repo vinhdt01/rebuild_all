6fe8aeeefb3c8ce2ed280b34619cdbf053e5ec73
vinh <daothevinh.tv@gmail.com>
2023-02-10 14:22:40 +0700

    asdasdasd

--- src/components/common/search-box.tsx
+++ src/components/common/search-box.tsx
@@ -67,42 +67,35 @@ const SearchBox = React.forwardRef<HTMLInputElement, SearchProps>(
 	({ className, onSubmit, onClear, ...rest }, ref) => {
 		const { t } = useTranslation("forms");
 		return (
-			// <form
-			// 	className="relative ltr:pr-12 rtl:pl-12 ltr:md:pr-14 rtl:md:pl-14 bg-white overflow-hidden rounded-md w-full"
-			// 	noValidate
-			// 	role="search"
-			// 	onSubmit={onSubmit}
-			// >
-			// 	<label htmlFor="search" className="flex items-center">
-			// 		<span className="w-12 md:w-14 h-full flex flex-shrink-0 justify-center items-center cursor-pointer focus:outline-none">
-			// 			<SearchIcon color="text-heading" className="w-4 h-4" />
-			// 		</span>
-			// 		<input
-			// 			id="search"
-			// 			className="text-heading outline-none w-full h-12 lg:h-14 placeholder-gray-400 text-sm lg:text-base leading-normal"
-			// 			placeholder="Tìm kiếm nhanh"
-			// 			aria-label="Search"
-			// 			autoComplete="off"
-			// 			ref={ref}
-			// 			{...rest}
-			// 		/>
-			// 	</label>
-			// 	<button
-			// 		type="button"
-			// 		className="outline-none text-2xl md:text-3xl text-gray-400 absolute top-0 ltr:right-0 rtl:left-0 w-12 md:w-14 h-full flex items-center justify-center transition duration-200 ease-in-out hover:text-heading focus:outline-none"
-			// 		onClick={onClear}
-			// 	>
-			// 		<IoCloseOutline className="w-6 h-6" />
-			// 	</button>
-			// </form>
-			<input type="text"
-						className="h-12"
+			<form
+				className="relative ltr:pr-12 rtl:pl-12 ltr:md:pr-14 rtl:md:pl-14 bg-white overflow-hidden rounded-md w-full"
+				noValidate
+				role="search"
+				onSubmit={onSubmit}
+			>
+				<label htmlFor="search" className="flex items-center">
+					<span className="w-12 md:w-14 h-full flex flex-shrink-0 justify-center items-center cursor-pointer focus:outline-none">
+						<SearchIcon color="text-heading" className="w-4 h-4" />
+					</span>
+					<input
+						id="search"
+						className="text-heading outline-none w-full h-12 lg:h-14 placeholder-gray-400 text-sm lg:text-base leading-normal"
 						placeholder="Tìm kiếm nhanh"
 						aria-label="Search"
 						autoComplete="off"
 						ref={ref}
 						{...rest}
-			/>
+					/>
+				</label>
+				<button
+					type="button"
+					className="outline-none text-2xl md:text-3xl text-gray-400 absolute top-0 ltr:right-0 rtl:left-0 w-12 md:w-14 h-full flex items-center justify-center transition duration-200 ease-in-out hover:text-heading focus:outline-none"
+					onClick={onClear}
+				>
+					<IoCloseOutline className="w-6 h-6" />
+				</button>
+			</form>
+			
 		);
 	}
 );
